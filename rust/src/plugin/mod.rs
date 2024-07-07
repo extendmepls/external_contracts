03ad8877714f4f000b7a6291dcfc324ef5ec121e
Obito-git <antonmyroshnychenko@gmail.com>
2024-07-07 19:48:09 +0200

    trying to fix the macros -_-

--- rust/src/plugin/mod.rs
+++ rust/src/plugin/mod.rs
@@ -1,10 +1,8 @@
-use std::any::Any;
-
 pub struct Context {
     pub data_example: String,
 }
 
-pub trait Plugin: Any + Send + Sync {
+pub trait Plugin: std::any::Any + Send + Sync {
     fn name(&self) -> &str;
     fn run(&self, ctx: &Context);
 }
@@ -15,11 +13,11 @@ pub struct PluginWrapper(pub Box<dyn Plugin>);
 macro_rules! declare_app_extend {
     ($app:ty, $constructor:path) => {
         #[no_mangle]
-        pub extern "C" fn _app_extend_create() -> *mut $crate::PluginWrapper {
+        pub extern "C" fn _app_extend_create() -> *mut $crate::plugin::PluginWrapper {
             let constructor: fn() -> $app = $constructor;
             let object = constructor();
-            let boxed: Box<dyn $crate::Plugin> = Box::new(object);
-            let wrapper = external_contracts::PluginWrapper(boxed);
+            let boxed: Box<dyn $crate::plugin::Plugin> = Box::new(object);
+            let wrapper = $crate::plugin::PluginWrapper(boxed);
             Box::into_raw(Box::new(wrapper))
         }
     };
