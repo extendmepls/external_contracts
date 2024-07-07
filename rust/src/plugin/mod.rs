e602c786b36c0b42a15617a690b97cf8da794fd9
Obito-git <antonmyroshnychenko@gmail.com>
2024-07-07 19:33:21 +0200

    add macros to replace boilerplate for rust extentions

--- rust/src/plugin/mod.rs
+++ rust/src/plugin/mod.rs
@@ -8,3 +8,19 @@ pub trait Plugin: Any + Send + Sync {
     fn name(&self) -> &str;
     fn run(&self, ctx: &Context);
 }
+
+pub struct PluginWrapper(pub Box<dyn Plugin>);
+
+#[macro_export]
+macro_rules! declare_app_extend {
+    ($app:ty, $constructor:path) => {
+        #[no_mangle]
+        pub extern "C" fn _app_extend_create() -> *mut $crate::PluginWrapper {
+            let constructor: fn() -> $app = $constructor;
+            let object = constructor();
+            let boxed: Box<dyn $crate::Plugin> = Box::new(object);
+            let wrapper = $crate::PluginWrapper(boxed);
+            Box::into_raw(Box::new(wrapper))
+        }
+    };
+}
