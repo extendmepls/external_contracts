3dd579129f54ce1dd4db2ca1b80f6beb5d137d6e
Obito-git <antonmyroshnychenko@gmail.com>
2024-07-17 22:13:20 +0200

    chore: Add debug information to FileNode struct

--- rust/src/data_source.rs
+++ rust/src/data_source.rs
@@ -3,15 +3,17 @@ use std::path::PathBuf;
 pub type OnChangedCallback = fn();
 
 #[derive(Debug)]
+#[allow(dead_code)]
 pub struct FileNode {
     name: String,
     path: PathBuf,
     is_dir: bool,
     size: u64,
-    data: Option<Vec<u8>>,
+    data: Result<Option<Vec<u8>>, DataSourceError>,
     children: Vec<FileNode>,
 }
 
+#[derive(Debug)]
 pub enum DataSourceError {
     NotFound,
     PermissionDenied,
@@ -21,5 +23,5 @@ pub enum DataSourceError {
 pub trait DataSource {
     fn watch(&self, on_changed: OnChangedCallback) -> Result<(), DataSourceError>;
     //TODO: return a mut/immutable reference to the data, to not stote it on the back and front on the same time
-    fn get_all_data(&self) -> Result<FileNode, DataSourceError>;
+    fn open(&self, path: PathBuf) -> &FileNode;
 }
