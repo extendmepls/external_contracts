4865dff9922049775fe01bb7b42324cf388e3a21
Obito-git <antonmyroshnychenko@gmail.com>
2024-07-17 23:12:34 +0200

    refactor FileNode struct to include data and children in constructor

--- rust/src/data_source.rs
+++ rust/src/data_source.rs
@@ -15,6 +15,29 @@ pub struct FileNode {
     children: Vec<FileNode>,
 }
 
+impl FileNode {
+    pub fn new(name: String, path: PathBuf, is_dir: bool, size: u64) -> Self {
+        Self {
+            name,
+            path,
+            is_dir,
+            size,
+            data: Err(DataSourceError::NotFound),
+            children: Vec::new(),
+        }
+    }
+
+    pub fn with_data(mut self, data: Result<Option<Vec<u8>>, DataSourceError>) -> Self {
+        self.data = data;
+        self
+    }
+
+    pub fn with_children(mut self, children: Vec<FileNode>) -> Self {
+        self.children = children;
+        self
+    }
+}
+
 #[derive(Debug)]
 pub enum DataSourceError {
     NotFound,
@@ -26,5 +49,5 @@ pub enum DataSourceError {
 pub trait DataSource {
     fn watch(&self, on_changed: OnChangedCallback) -> Result<(), DataSourceError>;
     //TODO: return a mut/immutable reference to the data, to not stote it on the back and front on the same time
-    async fn open(&self, path: PathBuf) -> FileNode;
+    async fn open(&self, path: PathBuf) -> Result<FileNode, DataSourceError>;
 }
